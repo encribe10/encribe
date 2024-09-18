58ad994f248f1c74c98c7bc684691b7073537247
encribe10 <ch.frick@gmx.de>
2024-09-18 11:20:58 +0200

    Create I<3U.bat

---
+++ I<3U.bat
@@ -0,0 +1,22 @@
+@echo off
+color 4
+echo.
+echo	                    ITTTTI                  OOOOO     OOOOO                UUUU          UUUU
+echo	                    I    I               OOO     O   O     OOO             U  U          U  U
+echo	                    I    I             OO          O         OO            U  U          U  U
+echo	                    I    I            OO                      OO           U  U          U  U
+echo	                    I    I            OO                      OO           U  U          U  U
+echo	                    I    I            OO                      OO           U  U          U  U
+echo	                    I    I            OO                      OO           U  U          U  U 
+echo	                    I    I              OO                  OO             U  U          U  U
+echo	                    I    I               OO                OO              U  U          U  U
+echo	                    I    I                OO              OO               U  U          U  U
+echo	                    I    I                  OO           OO                U  U          U  U
+echo	                    I    I                    OO        OO                 U  U          U  U
+echo	                    I    I                      OO    OO                   U   UUUUUUUUUU   U
+echo	                    I    I                       OO  OO                     U              U
+echo	                    ITTTTI                         OO                        UUUUUUUUUUUUUU
+echo.
+echo                                                                                                                ~Chris
+echo.
+pause
