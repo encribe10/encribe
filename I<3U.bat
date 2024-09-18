e8b2087b6fcb78e7936eaa0340111a7c6f4bbfd3
encribe10 <ch.frick@gmx.de>
2024-09-18 11:25:01 +0200

    Update I<3U.bat

--- I<3U.bat
+++ I<3U.bat
@@ -2,20 +2,35 @@
 color 4
 echo.
 echo	                    ITTTTI                  OOOOO     OOOOO                UUUU          UUUU
+sleep 1
 echo	                    I    I               OOO     O   O     OOO             U  U          U  U
+sleep 1
 echo	                    I    I             OO          O         OO            U  U          U  U
+sleep 1
 echo	                    I    I            OO                      OO           U  U          U  U
+sleep 1
 echo	                    I    I            OO                      OO           U  U          U  U
+sleep 1
 echo	                    I    I            OO                      OO           U  U          U  U
+sleep 1
 echo	                    I    I            OO                      OO           U  U          U  U 
+sleep 1
 echo	                    I    I              OO                  OO             U  U          U  U
+sleep 1
 echo	                    I    I               OO                OO              U  U          U  U
+sleep 1
 echo	                    I    I                OO              OO               U  U          U  U
+sleep 1
 echo	                    I    I                  OO           OO                U  U          U  U
+sleep 1
 echo	                    I    I                    OO        OO                 U  U          U  U
+sleep 1
 echo	                    I    I                      OO    OO                   U   UUUUUUUUUU   U
+sleep 1
 echo	                    I    I                       OO  OO                     U              U
+sleep 1
 echo	                    ITTTTI                         OO                        UUUUUUUUUUUUUU
+sleep 1
 echo.
 echo                                                                                                                ~Chris
 echo.
